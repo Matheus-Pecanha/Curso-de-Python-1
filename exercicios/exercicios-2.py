e736ae2cc6663df18302e78c4e4cd72206dff4ff
Matheus-Pecanha <matheusdasilva.pecanha@hotmail.com>
2026-07-27 21:38:45 -0300

    inicializando 2 lista

---
+++ exercicios/exercicios-2.py
@@ -0,0 +1,29 @@
+# Programa que mostre a frase "Olá mundo"
+# print("Olá mundo")
+
+# # Programa que leia o nome da pessoa e de as boas-vindas a ela
+# nome = input('Digite seu nome completo: ')
+
+# print('Olá', nome, 'é um prazer te conhecer!')
+
+# # Crie um programa que leia o nome e o sálario de um funcionário
+# nome = input('Digite seu nome: ')
+# salario = float(input('Digite seu sálario: '))
+
+# print(f'O(a) funcionário(a) {nome} tem um sálario de R${salario} em Junho')
+
+# Algoritmo que leia dois números inteiros e some
+
+# numero = int(input('Digite um número inteiro: '))
+# outro_numero = int(input('Digite outro número inteiro: '))
+# soma = numero + outro_numero
+
+# print(f'A soma entre {numero} e {outro_numero} é igual a {soma}')
+
+# Calculando duas notas e sua média
+
+nota1 = float(input('Digite sua primeira nota: '))
+nota2 = float(input('Digite sua segunda nota: '))
+media = nota1 + nota2 /2
+
+print(f'A média entra {nota1} e {nota2} é igual a', round(media,2))
