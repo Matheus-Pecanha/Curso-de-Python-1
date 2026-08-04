c3188d6e61dd66d5b5455331c578663e826f3c39
Matheus-Pecanha <matheusdasilva.pecanha@hotmail.com>
2026-08-04 10:59:37 -0300

    incompleto

--- 03-estrutura-repeticao/exercicios/exe04.py
+++ 03-estrutura-repeticao/exercicios/exe04.py
@@ -3,11 +3,19 @@
 contador = 0
 print(f'Quantidade de itens: {contador}')
 
-while contador <= 10:
-    adicionar_item = int(input('Digite a quantidade de itens que deseja adicionar: '))
-    contador += adicionar_item
+while contador < 10:
+    adicionando_itens = int(input('Digite a quantidade de itens que deseja adicionar: '))
+        
+    if adicionando_itens >= contador:
+        contador += adicionando_itens
+        print(f'A quantidade atual de itens é de {contador}')
 
-    if adicionar_item >= contador:
-        contador =+ adicionar_item
-        print(f'A quantidade atual de itens é de {adicionar_item}')
+    if adicionando_itens >= 10:
+        break    
 
+    
+       
+        
+
+else:
+    print('Você ultrapassou o limite máxio de itens')    
