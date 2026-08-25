9212da6605d22159f968744f69a069b11dff7d89
Matheus-Pecanha <matheusdasilva.pecanha@hotmail.com>
2026-08-24 21:39:11 -0300

    dicionario 24-08

---
+++ 06 - dicionários/exercicios/exe01.py
@@ -0,0 +1,24 @@
+produtos = [
+    {
+        'nome': 'Notebook',
+        'preço': 4500
+    },
+    {
+        'nome':'Celular'
+        'preço': 1800.50
+    },
+    {
+        'Nome': 'Mouse'
+        'preço': 45
+    },
+    {
+        'nome':'Monitor'
+        'preço': 670
+    },
+    {
+        'nome': 'Teclado'
+        'preço': 67
+    },
+]
+
+# Somar todos os valores dos produtos.
